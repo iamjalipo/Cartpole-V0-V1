18f651a70865658104a36a88d3a7db331e7986d4
mohammadjalal pouromid <55076585+iamjalipo@users.noreply.github.com>
2021-03-31 21:39:50 +0430

    new

--- DQN.py
+++ DQN.py
@@ -6,6 +6,7 @@ observation = env.reset()
 for _ in range(1000):
     env.render()
     time.sleep(0.1)
+    import ipdb ; ipdb.set_trace()
     action = env.action_space.sample()
     observation, reward , done , info = env.step(action)
     
