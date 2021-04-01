577ec856d92e4931012d78a3ef55c9d586580439
mohammadjalal pouromid <55076585+iamjalipo@users.noreply.github.com>
2021-04-01 08:49:43 +0430

    new

--- DQN.py
+++ DQN.py
@@ -8,6 +8,8 @@ for _ in range(1000):
     time.sleep(0.1)
     import ipdb ; ipdb.set_trace()
     action = env.action_space.sample()
+    #env.action_space.n get number of action
+    #env.observation_space.shape to get shape of observation
     observation, reward , done , info = env.step(action)
     
     if done:
