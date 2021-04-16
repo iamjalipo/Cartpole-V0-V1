06b6d1ad10e68ea8e561cff11668c34e93f5583b
mohammadjalal pouromid <55076585+iamjalipo@users.noreply.github.com>
2021-04-16 10:37:47 +0430

    n

--- deepPyTorch.py
+++ deepPyTorch.py
@@ -2,14 +2,15 @@ import torch
 
 from torch import nn
 
-s = nn.Sequential(
-    nn.Linear(2,5),
-    nn.ReLU(),
-    nn.Linear(5,7),
-    nn.ReLU(),
-    nn.Linear(7,10),
-    nn.Dropout(p = 0.3),
-    nn.Softmax(dim=1)
-)
-
-print(s(torch.Tensor([[2,5]])))
+class firstmodule(nn.Module):
+    def __init__(self , num_inputs , num_classes , dropout_prob = 0.3):
+        super(firstmodule , self).__init__
+        self.pipe = nn.Sequential(
+            nn.Linear(num_inputs, 5),
+            nn.ReLU(),
+            nn.Linear(5, 20),
+            nn.ReLU(),
+            nn.Linear(20, num_classes),
+            nn.Dropout(p=dropout_prob),
+            nn.Softmax(dim=1)
+        )
