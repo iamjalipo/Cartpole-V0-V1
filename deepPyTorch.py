bc83624eeb6bca088a146c0778ef68d2e9d6c2c0
mohammadjalal pouromid <55076585+iamjalipo@users.noreply.github.com>
2021-04-16 11:18:09 +0430

    6

--- deepPyTorch.py
+++ deepPyTorch.py
@@ -4,7 +4,7 @@ from torch import nn
 
 class firstmodule(nn.Module):
     def __init__(self , num_inputs , num_classes , dropout_prob = 0.3):
-        super(firstmodule , self).__init__
+        super(firstmodule , self).__init__()
         self.pipe = nn.Sequential(
             nn.Linear(num_inputs, 5),
             nn.ReLU(),
@@ -14,3 +14,12 @@ class firstmodule(nn.Module):
             nn.Dropout(p=dropout_prob),
             nn.Softmax(dim=1)
         )
+    def forward(self , x):
+        return self.pipe(x)
+
+if __name__ == "__main__":
+    net = firstmodule(num_inputs=2, num_classes=3)
+    v = torch.FloatTensor([[2, 3]])
+    out = net(v)
+    print(net)
+    print(out)
