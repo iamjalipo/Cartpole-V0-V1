0bbb1587a3bc42de5c8556567f5b7d996389f1ec
mohammadjalal pouromid <55076585+iamjalipo@users.noreply.github.com>
2021-04-16 10:27:26 +0430

    h

--- deepPyTorch.py
+++ deepPyTorch.py
@@ -1,6 +1,15 @@
 import torch
 
-a = torch.tensor([1,2,3])
+from torch import nn
 
-x = a.sum()
-print(x.item())
+s = nn.Sequential(
+    nn.Linear(2,5),
+    nn.ReLU(),
+    nn.Linear(5,7),
+    nn.ReLU(),
+    nn.Linear(7,10),
+    nn.Dropout(p = 0.3),
+    nn.Softmax(dim=1)
+)
+
+print(s(torch.Tensor([[2,5]])))
